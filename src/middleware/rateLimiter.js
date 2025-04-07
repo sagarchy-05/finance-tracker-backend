0e43d07457810e47b1cb170f6c4307b551f91be3
Sagar Kumar Choudhary <sagarchoudharyaot@gmail.com>
2025-04-07 18:13:57 +0530

    added try-again-after

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,10 +1,19 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
+  windowMs: 60 * 60 * 1000, // 1 hour
   max: 3,
-  message: {
-    message: 'Too many requests. Please try again after an hour.',
+  handler: (req, res) => {
+    const now = new Date();
+    const resetTime =
+      req.rateLimit.resetTime || new Date(now.getTime() + 60 * 60 * 1000); // fallback
+    const retryAfter = Math.ceil((resetTime - now) / 1000); // in seconds
+
+    res.set('Retry-After', retryAfter);
+    res.status(429).json({
+      message: 'Rate limit exceeded. Please try again later.',
+      retryAfter, // in seconds
+    });
   },
   standardHeaders: true,
   legacyHeaders: false,
